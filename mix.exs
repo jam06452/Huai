9c8b63810b463e5bd65115b0d3dfdbd75b1ce084
q <jam@jam06452.uk>
2026-06-01 07:50:05 +0100

    Added snex

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Huai.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:snex, "~> 0.4.2"}
     ]
   end
 
